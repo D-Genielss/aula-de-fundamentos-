d2b8bacec8e4a1384f6614be6696e708addade93
D-Genielss <danielmartin@alunos.utfpr.edu.br>
2025-06-17 09:51:08 -0300

    Add files via upload

---
+++ aula de fundamentos da programacao/exercicios/exercicios aula 14/exercicio 3 a14.c
@@ -0,0 +1,29 @@
+/*Escreva um procedimento que recebe por parâmetro as 3 notas de um
+aluno e uma letra. Se a letra for A, o procedimento calcula a média
+aritmética das notas do aluno, se for P, a sua média ponderada (pesos: 5, 3
+e 2) e se for S, a soma das notas. O valor calculado também deve ser
+retornado e exibido na função main.*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int main (){
+
+    //variaveis
+
+    int nota1,nota2,nota3;
+    char letra;
+    
+    //testando 
+
+    if(letra == 'a' || letra == 'A'){
+
+    }else if(letra == 'p' || letra == 'P'){
+
+    }else if(letra == 's' || letra == 'S'){
+        
+    }else{
+
+    }
+}
