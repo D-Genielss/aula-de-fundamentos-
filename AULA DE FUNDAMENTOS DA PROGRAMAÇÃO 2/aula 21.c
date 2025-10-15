990c88c31c53d77cb4cb39ccca616628b1988e4b
D-Genielss <danielmartin@alunos.utfpr.edu.br>
2025-10-15 17:49:14 -0300

    Create aula 21.c

---
+++ AULA DE FUNDAMENTOS DA PROGRAMAÇÃO 2/aula 21.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+    //char texto[30] = "Apucarana";
+
+    //FILE* arquivo = fopen("./texto.txt","w");
+
+    //if(arquivo == NULL){
+
+    //    perror("Error ao abrir arquivo : ");
+    //    Exit(1);
+
+    //}
+
+    printf("arquivo aberto com sucesso \n");
+
+    //for(int i = 0 ; i < strlen(texto);i ++){
+
+    //fputc(texto[i] , arquivo);
+
+    //}
+
+    //fclose(arquivo);
+    
+    return 0;
+}
